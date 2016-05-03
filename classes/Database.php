f3db202517fd95ad0a1073a63c9d2564e88b2f85
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2016-05-03 20:06:37 -0300

    Alterando Database

--- classes/Database.php
+++ classes/Database.php
@@ -1,42 +1,32 @@
 <?php
 
-// CLASSE DE BANCO DE DADOS
-// Criada em 27 de abril de 2016
-// Versão 1 de 27 de abril de 2016
-
-class Database {
+abstract class Database extends PDO
+{
+	private $dns     = 'mysql:host=localhost;dbname=h3s7s_secfatec;';//dns na sequencia, driver, host, banco de dados
+	private $user    = 'root';//usuário do banco de dados
+	private $pass    = '';//senha do banco de dados
+    private $charset = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];//evitando erros de acentuação
 	
-	static public function connect(){
-		
-		$host = "localhost";
-		$name = "h3s7s_secfatec";
-		$user = "h3s7s_secfatec";
-		$pass = "%kv+3-,^4IBW";
-		
-		try {
-        	
-        	$db = new PDO("mysql:host=" . $host . ";dbname=" . $name, $user, $pass);
-			
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-			
-            return $db;
+	function __construct()
+	{
+		try 
+		{
+			parent::__construct($this->dns,$this->user,$this->pass,$this->charset);
+			parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			parent::setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-            //eh neh
-
-        } catch(PDOException $e) {
-			
-			$arr['titulo'] = 'Erro de Conexão com o Banco de Dados!';
-			$arr['msg'] = 'Um erro inesperado de conexão está ocorrendo com o sistema! 
-				Não é possível realizar solicitações de dados. Verifique seu acesso à internet para continuar.';
-			$arr['info'] = 'Informações sobre o erro para suporte primenote: Class DB | ' . $e->getMessage();
-			
-			$retorno[erro][] = $arr;
-			return $retorno;
-			die();
-
-        }
-
+			return TRUE;
+		}
+		catch(PDOException $e) 
+		{
+			return json_encode([
+								"message" => $e->getMessage(), 
+								"code" => $e->getCode(), 
+								"file" => $e->getFile(), 
+								"lineError" => $e->getLine()
+							]);
+		}
 	}
-	
 }
+
+?>
