d64548eab38dfffdb5590b2b2f468b6821652d62
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2016-05-04 21:07:02 -0300

    Comentários

--- sys/Denuncias.php
+++ sys/Denuncias.php
@@ -12,6 +12,12 @@ require_once(dirname(dirname(__FILE__)) . '/core/autoload.php');
 					$db = new Database();
 					$query = $db->prepare("SELECT * FROM reclamantes");
 					$query->execute();
+					/*
+						Um breve exemplo utilizando uma query, 
+						a seguir criarei um .sql, 
+						contendo os pontos no mapa, 
+						e o id dos reclamantes[cabe alteração de formulário];
+					*/
 
 					echo json_encode($query->fetchAll(PDO::FETCH_ASSOC));
 
